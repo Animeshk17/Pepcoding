0a45f2db4f949deceb7a7a1a2ec696c45081f5e0
Animeshk17 <54817804+Animeshk17@users.noreply.github.com>
2021-08-01 17:43:42 +0530

    Added Extended Euclidean Algorithm

---
+++ Advanced/Number Theory/Extended_Euclidean_Algorithm.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Pair{
+    public:
+    int x;
+    int y;
+
+    Pair(){
+
+    }
+
+    Pair(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+};
+
+Pair extendedEuclidean(int a, int b){ //pair.x contains x and pair.y contains y
+    if(b==0) return Pair(1,0);
+
+    Pair nextLevel = extendedEuclidean(b,a%b);
+
+    Pair ans;
+    ans.x = nextLevel.y;
+    ans.y = nextLevel.x - (a/b) * nextLevel.y;
+    return ans;
+
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    Pair soln = extendedEuclidean(a,b);
+
+    cout << soln.x << " " << soln.y << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+This algorithm is used to find integral solutions of the equation: ax + by = gcd(a,b)
+
+By reversing the steps in the Euclidean algorithm, it is possible to find these integers a and b. 
+The whole idea is to start with the GCD and recursively work our way backwards. 
+This can be done by treating the numbers as variables until we end up with an expression that is a linear combination of our initial numbers. 
+
+Read More at: https://brilliant.org/wiki/extended-euclidean-algorithm/#
+*/
