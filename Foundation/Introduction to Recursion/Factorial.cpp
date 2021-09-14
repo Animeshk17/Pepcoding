33a217f3fbd76d5aaa64a6fcc9e7cef7cf9c92a5
Animeshk17 <54817804+Animeshk17@users.noreply.github.com>
2021-09-14 20:50:22 +0530

    Added Recursive Solution For Calculating Factorial

---
+++ Foundation/Introduction to Recursion/Factorial.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+unsigned int factorial(unsigned int n){
+
+    if(n==0) return 1;
+    
+    return n * factorial(n-1);
+    
+}
+
+int main(){
+
+    unsigned int n;
+    cin>>n;
+    
+    cout<< factorial(n) << "\n";
+}
