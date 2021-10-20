0b1d1e712047b8e8545dbfca949d250b14547f41
Animeshk17 <54817804+Animeshk17@users.noreply.github.com>
2021-10-20 20:01:50 +0530

    Optimised Solution For "Squares Of Sorted Array"

--- LevelUp/Arrays and Strings/Squares_Of_Sorted_Array.cpp
+++ LevelUp/Arrays and Strings/Squares_Of_Sorted_Array.cpp
@@ -8,13 +8,49 @@ void print(vector<int> & a){
 
 }
 
-void sortSquares(vector<int>& a, int n){
+void mergeTwoSortedArrays(vector<int>& arr1, vector<int>& arr2, vector<int>& a, int n){
 
-    for(int i = 0; i<n; i++){
-        a[i] = a[i] * a[i];
+    int i = 0, j = 0, k = 0;
+
+    while(i < arr1.size() && j < arr2.size()){
+        
+        if(arr1[i] <= arr2[j]){
+        
+            a[k++] = arr1[i++];
+        
+        }else{
+
+            a[k++] = arr2[j++];
+
+        }
+
     }
 
-    sort(a.begin(), a.end());
+    while(i < arr1.size()){
+    
+        a[k++] = arr1[i++];
+    
+    }
+
+    while(j < arr2.size()){
+    
+        a[k++] = arr2[j++];
+    
+    }
+}
+
+void sortSquares(vector<int>& a, int n){
+
+    int positivesStartIndex = lower_bound(a.begin(), a.end(), 0) - a.begin();
+
+    vector<int> arr1;
+    for(int i = positivesStartIndex - 1; i>=0; i--) arr1.push_back(a[i]*a[i]);
+
+    vector<int> arr2;
+    for(int i = positivesStartIndex; i<n; i++) arr2.push_back(a[i] * a[i]);
+
+    mergeTwoSortedArrays(arr1, arr2, a, n);
+    return ;
 
 }
 
